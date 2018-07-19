42ad2c3afdbdbc2d5f40f29e0bf99579eb55f6b6
Craig Wann, Eric Shull and Nick Brown <nick@>
2018-07-19 10:12:17 -0700

    add callbacks for employees and projects

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,12 @@
 class Employee < ActiveRecord::Base
   has_and_belongs_to_many(:projects)
   validates(:name, :presence => true)
+  before_save(:titlecase_name)
 
 
+  private
+
+  def titlecase_name
+    self.name = name.capitalize()
+  end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,12 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many(:employees)
   validates(:name, :presence => true, :length => { :maximum => 50})
+  before_save(:downcase_name)
 
+
+private
+  def downcase_name
+    self.name=(name.downcase())
+  end
 
 end
